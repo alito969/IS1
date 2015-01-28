baf0ef80fe6fd2d01a226d76466795da1174318d
Cecilia <Cecilia@Cecilia-PC>
2015-01-28 03:03:10 -0400

    imprime hola mundo

--- DigitalHisClinica/src/digitalhisclinica/Principal.java
+++ DigitalHisClinica/src/digitalhisclinica/Principal.java
@@ -59,7 +59,8 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+
+        System.out.println("HOLA MUNDO");// TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**

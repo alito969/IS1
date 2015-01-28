ba15767638aafc9a79121cfb96556c4ef60e98c2
Cecilia <Cecilia@Cecilia-PC>
2015-01-28 03:55:25 -0400

    nada

--- DigitalHisClinica/src/digitalhisclinica/Principal.java
+++ DigitalHisClinica/src/digitalhisclinica/Principal.java
@@ -59,8 +59,7 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        System.out.println("Nueva linea en mi rama");
-        System.out.println("probar el push request");// TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
